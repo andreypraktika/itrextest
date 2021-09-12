33ce844c67b8b84c5dde51e81d37cad14e85f53f
andreypraktika <andrey_praktika@mail.ru>
2021-09-12 13:20:25 +0300

    improved css

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import "../pagination.css";
+import { setCurrentPage } from "../Redux/redux-reducers";
+import { connect } from "react-redux";
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+
+const Pagination = (props) => {
+  const { postsPerPage, totalPosts, paginate, currentPage } = props;
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+  const lastPage = Math.ceil(totalPosts / postsPerPage)
+  for (let i = 1; i <= lastPage; i++) {
     pageNumbers.push(i);
   }
 
   return (
     <nav>
       <ul className='pagination'>
+        <li>
+        <a hidden={currentPage === 1 && true} onClick={() => {
+          console.log(currentPage);
+          props.setCurrentPage(currentPage - 1)
+          console.log(currentPage);
+          }} href='!#' className='page-link' >Prev</a>
+        </li>
         {pageNumbers.map(number => (
           <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link' id = {number} >
+            <a onClick={() => paginate(number)} href='!#' className='page-link' id={currentPage === number && 'active'} >
               {number}
             </a>
           </li>
         ))}
+        <li>
+        <a hidden={ currentPage === lastPage && true} onClick={() => {
+          props.setCurrentPage(currentPage + 1)
+          }} href='!#' className='page-link' >Next</a>
+        </li>
       </ul>
     </nav>
   );
 };
 
-export default Pagination;
+const mapStateToProps = (state) => ({
+  currentPage: state.currentPage,
+});
+
+export default connect(mapStateToProps, { setCurrentPage })(Pagination);
+
